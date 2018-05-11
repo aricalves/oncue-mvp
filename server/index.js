7e1c1844e45fe633c35326b5d762afb13c60e8b8
Aric Alves <aric.alves@yahoo.com>
2018-05-11 14:53:43 -0700

    Add routes and pseudocode for each

--- server/index.js
+++ server/index.js
@@ -8,15 +8,46 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const router = express.Router();
+// const router = express.Router();
 
 const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 
 app.use(staticFiles);
 
-router.get('/ping', (req, res) => res.send('Pong'));
+app.post('/trucks', (req, res) => {
+  console.log(req.body)
+  // create new entry in db
+  // send trucks w/ jobs back to client
+  res.send('ok');
+});
 
-app.use(router);
+app.post('/jobs', (req, res) => {
+  console.log(req.body);
+  // if no trucks exist
+    // send err('No Trucks available')
+    // Status code 200
+  // if trucks exist without jobs, assign job to first truck
+    // send client all trucks with jobs; code: 200
+  // if all trucks have jobs
+    // compare date, start time, and duration to each truck's assigned jobs
+    // if we find a truck with an open slot
+      // asssign the job to the truck
+      // send client updated trucks w/ jobs list; code:200
+  // else
+    // send client an error, job cannot be created 'something went wrong'
+  res.send('ok');
+});
+
+app.delete('/jobs', (req, res) => {
+  console.log(req.body);
+  // find job by id
+  // if not found, send err
+  // else delete job from db and remove reference from job's assigned truck
+  // send client updated trucks w/ jobs list
+  res.send('ok');
+});
+
+// app.use(router);
 
 app.use('/*', staticFiles);
 
