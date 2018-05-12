63e773680ef469313a5af2ff5a8a578b4cde92db
Aric Alves <aric.alves@yahoo.com>
2018-05-11 21:55:34 -0700

    Add date and time formatting

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+const parseTime = (military) => {
+  if (military <= 900) {
+    return `${military * 0.01} am`
+  }
+  if (military > 1200) {
+    return `${(military - 1200) * 0.01} pm`
+  }
+  return `${military * 0.01} am`
+};
+
 const Job = (props) => (
   <div className='job-container'>
     <p>{`${props.name} Assignments`}</p>
@@ -7,7 +17,7 @@ const Job = (props) => (
       {props.jobs.map(job => {
         return (
           <li key={job.id}>
-            <span>Customer: {job.customer_name}, Date: {job.date} Start: {job.start_time} Duration: {job.duration} hours</span>
+            <span>Customer: {job.customer_name}, Date: {job.date.substring(0, 10)} Start: {parseTime(job.start_time)} Duration: {job.duration} hours</span>
             <span id='delete' onClick={(e) => props.handleDeleteJob(job.id, e)}>&times;</span>
           </li>
         )
