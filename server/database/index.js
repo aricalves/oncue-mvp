236d066833d8d03234a454eb15929c0364a0efa0
Aric Alves <aric.alves@yahoo.com>
2018-05-11 22:04:16 -0700

    Fix database uri

--- server/database/index.js
+++ server/database/index.js
@@ -6,7 +6,7 @@ let sequelize;
 if (process.env.NODE_ENV !== 'production') {
   sequelize = new Sequelize('oncue', process.env.DB_USER, process.env.DB_PASS, { host: process.env.DB_HOST, dialect: 'postgres' });
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URI);
 }
 
 sequelize.authenticate()
