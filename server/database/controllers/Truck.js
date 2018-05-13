423922a10ddab3e7136a6157c2f9e1a17ac9d21a
Aric Alves <aric.alves@yahoo.com>
2018-05-12 22:27:53 -0700

    Add query to filter trucks by hours

--- server/database/controllers/Truck.js
+++ server/database/controllers/Truck.js
@@ -1,6 +1,22 @@
-import { Truck, sequelize } from '../../database';
+import { Truck } from '../../database';
 
 exports.createTruck = newTruck => Truck.create(newTruck);
 
 exports.getAll = () => Truck.findAll({ include: 'jobs' })
   .then(trucks => trucks.map(truck => truck.dataValues));
+
+exports.findAvailableTruck = (start, end, conflicts = []) => Truck.findAll({
+  where: {
+    start_time: {
+      $lte: start
+    },
+    end_time: {
+      $gte: end
+    },
+    id: {
+      $notIn: conflicts
+    }
+  }
+}).spread(trucks => trucks ? trucks.dataValues : []);
+
+  
