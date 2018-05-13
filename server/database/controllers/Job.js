2554d8d122404919cbe3a6e1064ce114a8362ab6
Aric Alves <aric.alves@yahoo.com>
2018-05-13 00:42:27 -0700

    Remove commented code

--- server/database/controllers/Job.js
+++ server/database/controllers/Job.js
@@ -7,13 +7,6 @@ exports.createJob = newJob => Job.create(newJob);
 exports.getAll = () => Job.findAll()
   .then(jobs => jobs.map(job => job.dataValues));
 
-// exports.findConflicts = job => sequelize.query(
-//   `SELECT * FROM jobs j WHERE (j.date = '${job.date}'::date AND
-//     (j.start_time BETWEEN ${job.start_time} AND ${job.duration}) OR
-//     (j.duration BETWEEN ${job.start_time} AND ${job.duration}));`)
-//     .spread(conflicts => conflicts)
-//     .catch(err => err);
-
 exports.findConflicts = job => {
   const time_range = [];
   for (let i = job.start_time; i <= job.duration; i += 100) time_range.push(i);
