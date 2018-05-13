1d287d08586539277db109d6c0c8d61c7c5f516d
Aric Alves <aric.alves@yahoo.com>
2018-05-12 17:34:56 -0700

    Add SQL query for jobs

--- server/database/controllers/Job.js
+++ server/database/controllers/Job.js
@@ -1,6 +1,13 @@
-import { Job } from '../../database';
+import { Job, sequelize } from '../../database';
 
 exports.createJob = newJob => Job.create(newJob);
+
 exports.getAll = () => Job.findAll()
   .then(jobs => jobs.map(job => job.dataValues));
+
+exports.findJobsByDate = newJobDate =>
+  sequelize.query(`SELECT * FROM jobs WHERE date != '${newJobDate}'::date;`)
+    .then(res => res)
+    .catch(err => err);
+
 exports.deleteJob = id => Job.destroy({ where: { id } });
--- server/database/controllers/Truck.js
+++ server/database/controllers/Truck.js
@@ -1,5 +1,6 @@
-import { Truck } from '../../database';
+import { Truck, sequelize } from '../../database';
 
 exports.createTruck = newTruck => Truck.create(newTruck);
+
 exports.getAll = () => Truck.findAll({ include: 'jobs' })
   .then(trucks => trucks.map(truck => truck.dataValues));
